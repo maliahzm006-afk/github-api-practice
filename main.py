da0347780dc28c186de07bcde7df62250b85ff45
Ali Hamza <maliahzm006@gmail.com>
2026-08-30 09:43:24 +0500

    changes required

---
+++ main.py
@@ -0,0 +1,12 @@
+'''git hub learning + python tutorials'''
+
+print('git + git hub learning')
+
+print()
+
+number = input('Enter number: ')
+
+if number == '0':
+    print('basics to advanced...')
+else:
+    print('invalid right now...')
