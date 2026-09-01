6974b2dc3d2806328f47665274b94251efcb7ba9
Ali Hamza <maliahzm006@gmail.com>
2026-09-01 09:14:21 +0500

    github token Gen

---
+++ tokenGen.py
@@ -0,0 +1,23 @@
+'''GitHub Token Generating...'''
+
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("GITHUB_TOKEN")
+
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Accept": "application/vnd.github+json"
+}
+
+response = requests.get(
+    "https://api.github.com/user",
+    headers=headers
+)
+
+print(response.status_code)
+print(response.json())
+
